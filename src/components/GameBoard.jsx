42f3b92e8d0172596867ad475892b58ffec26ddb
Michelle <mpuschkarow@web.de>
2025-10-30 18:42:00 +0100

    lifted state up to app component in order to manage current player and change of player after clicking

--- src/components/GameBoard.jsx
+++ src/components/GameBoard.jsx
@@ -1,15 +1,28 @@
+import { useState } from "react"
+
 const initialGameBoard = [
     [null, null, null],
     [null, null, null],
     [null, null, null],
 ]
 
-export default function GameBoard() {
+export default function GameBoard( {onSelectSquare, currentPlayerSymbol} ) {
+    const [gameBoard, setGameBoard] = useState(initialGameBoard)
+
+    function handleSelectedSquare(rowIndex, colIndex){
+        setGameBoard((prevGameBoard) => {
+            const updatedBoard = [...prevGameBoard].map(colArray => [...colArray]);
+            updatedBoard[rowIndex][colIndex] = currentPlayerSymbol;
+            return updatedBoard
+        })
+        onSelectSquare()
+    }
+
     return <ol id="game-board">
-        { initialGameBoard.map( (row, rowIndex) => <li key={rowIndex}>
+        { gameBoard.map( (row, rowIndex) => <li key={rowIndex}>
             <ol>
                 {row.map( (playerSymbol, colIndex) => <li key={colIndex}>
-                    <button>{playerSymbol}</button>
+                    <button onClick={() => handleSelectedSquare(rowIndex, colIndex)}>{playerSymbol}</button>
                 </li>)}
             </ol>
         </li> ) }
