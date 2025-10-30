6f03df13b7d843e57fb554241195ac4100cec496
Michelle <mpuschkarow@web.de>
2025-10-30 17:06:27 +0100

    Two-way-binding of inputs by user using state and onChange event listener

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -1,18 +1,22 @@
 import { useState } from "react";
 
-export default function Player({name, symbol}) {
-
+export default function Player({initialName, symbol}) {
+    const [playerName, setPlayerName] = useState(initialName)
     const [isEditing, setIsEditing] = useState(false)
 
     function handleClick() {
         setIsEditing(state => !state)
     }
 
+    function handleChange(event) {
+        setPlayerName(event.target.value)
+    }
+
     return (
         <li>
             <span className="player">
-             {!isEditing && <span className="player-name">{name}</span>}
-             {isEditing && <input type="text" required />}
+             {!isEditing && <span className="player-name">{playerName}</span>}
+             {isEditing && <input type="text" required value={playerName} onChange={handleChange}/>}
               <span className="player-symbol">{symbol}</span>
             </span>
             <button onClick={handleClick}>{isEditing ? 'Save' : 'Edit'}</button>
