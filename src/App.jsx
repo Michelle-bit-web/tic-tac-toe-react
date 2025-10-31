cb876ef849c0bad5e777f0f8fd4595ec40cdc88d
Michelle <mpuschkarow@web.de>
2025-10-31 11:33:15 +0100

    Code refactored

--- src/App.jsx
+++ src/App.jsx
@@ -3,26 +3,30 @@ import GameBoard from "./components/GameBoard";
 import Log from "./components/Log";
 import { useState } from "react";
 
+function deriveActivePlayer(gameTurns) {
+   let currentPlayer = "X";
+      if (gameTurns.length > 0 && gameTurns[0].player === "X") {
+        currentPlayer = "O";
+      }
+    return currentPlayer
+}
+
 function App() {
-  const [gameTurns, setGameTurns] = useState([])
-  const [activePlayer, setActivePlayer] = useState('X')
+  const [gameTurns, setGameTurns] = useState([]);
+  const activePlayer = deriveActivePlayer(gameTurns)
 
   function handlePlayerChange(rowIndex, colIndex) {
-    setActivePlayer(currentPlayer => currentPlayer === "X" ? "O" : "X")
-    setGameTurns(prevTurns => {
-      let currentPlayer = 'X';
-      if (prevTurns.length > 0 && prevTurns[0].player === 'X'){
-        currentPlayer = 'O'
-      }
+    setGameTurns((prevTurns) => {
+      const currentPlayer = deriveActivePlayer(prevTurns)
       const updatedTurns = [
         {
-          square: {row: rowIndex, col: colIndex},
-          player: currentPlayer
-        }, 
-        ...prevTurns
-      ]
+          square: { row: rowIndex, col: colIndex },
+          player: currentPlayer,
+        },
+        ...prevTurns,
+      ];
 
-      return updatedTurns
+      return updatedTurns;
     });
   }
 
@@ -33,13 +37,9 @@ function App() {
           <Player initialName="Player 1" symbol="X" isActive={activePlayer === "X"}></Player>
           <Player initialName="Player 2" symbol="O" isActive={activePlayer === "O"}></Player>
         </ol>
-        <GameBoard 
-          onSelectSquare={handlePlayerChange} 
-          currentPlayerSymbol={activePlayer}
-          turns={gameTurns}
-        />
+        <GameBoard onSelectSquare={handlePlayerChange} currentPlayerSymbol={activePlayer} turns={gameTurns} />
       </div>
-      <Log />
+      <Log turns={gameTurns} />
     </main>
   );
 }
